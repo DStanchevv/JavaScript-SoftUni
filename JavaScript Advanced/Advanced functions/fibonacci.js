0d19496c042d55a981062af2d6c5461ea8c0496a
D. Stanchev <denislavstanchev23@gmail.com>
2022-10-03 12:16:09 +0300

    exercises

---
+++ JavaScript Advanced/Advanced functions/fibonacci.js
@@ -0,0 +1,11 @@
+function getFibunator(){
+    let firstNum = 0;
+    let secNum = 1;
+
+    return function(){
+        let sum = firstNum + secNum;
+        firstNum = secNum;
+        secNum = sum;
+        return firstNum;
+    }
+}
